cd608b96429423bedb999dee6dbb08d07bd45449
Keith A <keitha@unsabo.de>
2026-05-13 09:51:07 +0100

    Remove invalid federation config

--- web-app-radicale/vite.config.ts
+++ web-app-radicale/vite.config.ts
@@ -1,13 +1,5 @@
 import { defineConfig } from '@opencloud-eu/extension-sdk'
 
 export default defineConfig({
-  name: 'web-app-radicale',
-  federation: {
-    shared: {
-      'vue3-gettext': {
-        requiredVersion: false,
-        eager: true
-      }
-    }
-  }
+  name: 'web-app-radicale'
 })
